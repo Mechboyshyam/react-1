666652fd59c4e225377ce5f394bc499aba522b93
shyam <shyamkorade0@gmail.com>
2022-11-21 09:08:35 +0530

    Default and named function practice

---
+++ src/Two_function.js
@@ -0,0 +1,18 @@
+import './two_functionn.css';
+export function Twoo_function(){
+    return(
+        <>
+        <button className="button_1">Function_1</button>
+        </>
+    )
+}
+
+export function Function_second(){
+    return(
+        <>
+        <button className="button_2">Function_2</button>
+        </>
+    )
+}
+
+
